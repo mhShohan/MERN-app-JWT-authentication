9cda400b434d82a848b458d885386cb370f2ea64
mhshohan1 <mehdihasanshohan25@gmail.com>
2022-02-15 02:03:06 +0600

    UI design

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,63 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
 
 export default function Login() {
-    return <div>Login</div>;
+    const [formData, setFormDate] = useState({
+        email: '',
+        password: '',
+    });
+    const { email, password } = formData;
+
+    const onChange = (event) => {
+        setFormDate((prevState) => ({
+            ...prevState,
+            [event.target.name]: event.target.value,
+        }));
+    };
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <>
+            <section className="heading">
+                <h1>
+                    <FaSignInAlt />
+                    Login to your Acconut..
+                </h1>
+            </section>
+            <section className="form">
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="email"
+                            name="email"
+                            value={email}
+                            onChange={onChange}
+                            placeholder="Enter Your email"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
+                            name="password"
+                            value={password}
+                            onChange={onChange}
+                            placeholder="Enter password"
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-block">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </>
+    );
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,86 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { FaUser } from 'react-icons/fa';
 
 export default function Register() {
-    return <div>Register</div>;
+    const [formData, setFormDate] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+    const { name, email, password, confirmPassword } = formData;
+
+    const onChange = (event) => {
+        setFormDate((prevState) => ({
+            ...prevState,
+            [event.target.name]: event.target.value,
+        }));
+    };
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <>
+            <section className="heading">
+                <h1>
+                    <FaUser />
+                    Register An Acconut..
+                </h1>
+            </section>
+            <section className="form">
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="name"
+                            name="name"
+                            value={name}
+                            onChange={onChange}
+                            placeholder="Enter Your name"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="email"
+                            name="email"
+                            value={email}
+                            onChange={onChange}
+                            placeholder="Enter Your email"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
+                            name="password"
+                            value={password}
+                            onChange={onChange}
+                            placeholder="Enter password"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="confirmPassword"
+                            name="confirmPassword"
+                            value={confirmPassword}
+                            onChange={onChange}
+                            placeholder="Confirm Password"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-block">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </>
+    );
 }
